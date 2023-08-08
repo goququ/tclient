ef1a332fad74525105d8c9fc267d9019dfbe8b6c
Ruslan.Nasyrov <goququ@ya.ru>
2023-08-08 15:36:31 +0500

    continue

--- internal/server/create-chat.go
+++ internal/server/create-chat.go
@@ -1,51 +1,73 @@
 package server
 
 import (
-	"log"
+	"fmt"
 	"net/http"
 
+	"github.com/anonyindian/gotgproto"
 	"github.com/anonyindian/gotgproto/storage"
 	"github.com/gin-gonic/gin"
 	"github.com/gotd/td/tg"
 )
 
-func (h AppHandlers) createChat(c *gin.Context) {
-	creationContext := h.client.CreateContext()
+type chatsBundle struct {
+	chat     *tg.Chat
+	fullChat tg.ChatFullClass
+}
+
+func createTelegramChat(c *gotgproto.Client) (*chatsBundle, error) {
+	creationContext := c.CreateContext()
 	chat, err := creationContext.CreateChat("test", []int64{})
 	if err != nil {
-		c.AbortWithError(http.StatusInternalServerError, err).SetType(gin.ErrorTypePrivate)
-		return
+		return nil, fmt.Errorf("unable to create chat")
 	}
 
 	storage.AddPeer(chat.GetID(), storage.DefaultAccessHash, storage.TypeChat, storage.DefaultUsername)
 
 	fullChat, err := creationContext.GetChat(chat.GetID())
 	if err != nil {
-		log.Println("Unable to get full chat")
-		c.AbortWithError(http.StatusInternalServerError, err).SetType(gin.ErrorTypePrivate)
-		return
+		return nil, fmt.Errorf("unable to create chat")
 	}
 
-	invite, ok := fullChat.GetExportedInvite()
+	return &chatsBundle{
+		chat:     chat,
+		fullChat: fullChat,
+	}, nil
+}
+
+func getChatLink(c tg.ChatFullClass) (string, error) {
+	invite, ok := c.GetExportedInvite()
 	if !ok {
-		log.Println("Unable to get invite link")
-		c.AbortWithError(http.StatusInternalServerError, err).SetType(gin.ErrorTypePrivate)
-		return
+		return "", fmt.Errorf("unable to extract link from chat")
 	}
 
 	var link string
 	if exportedInvite, ok := invite.(*tg.ChatInviteExported); ok {
 		link = exportedInvite.Link
 	} else {
-		log.Println("Unable to get access to invite link field")
+		return "", fmt.Errorf("unable to get access to invite link field")
+	}
+
+	return link, nil
+}
+
+func (h AppHandlers) createChat(c *gin.Context) {
+	chatBundle, err := createTelegramChat(h.client)
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err).SetType(gin.ErrorTypePrivate)
+		return
+	}
+
+	link, err := getChatLink(chatBundle.fullChat)
+	if err != nil {
 		c.AbortWithError(http.StatusInternalServerError, err).SetType(gin.ErrorTypePrivate)
 		return
 	}
 
 	res := gin.H{
-		"chatID":   chat.GetID(),
-		"title":    chat.GetTitle(),
-		"joinLink": link,
+		"id":    chatBundle.fullChat.GetID(),
+		"title": chatBundle.chat.GetTitle(),
+		"link":  link,
 	}
 
 	c.JSON(http.StatusOK, res)
